4bf1df1ba95ecab321c73f62639dc6210280fb01
pema124 <peter.mathes@gmx.de>
2024-05-21 21:59:11 +0200

    schneller

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     basic.setLedColors(0xff0000, 0xff0000, 0xff0000)
-    basic.pause(500)
+    basic.pause(100)
     basic.setLedColors(0x00ff00, 0xff8000, 0x00ff00)
-    basic.pause(500)
+    basic.pause(100)
 })
