b80efdcba29da532abd146b2c25e6121153448e1
pema124 <peter.mathes@gmx.de>
2024-05-21 21:57:32 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    basic.setLedColors(0xff0000, 0xff0000, 0xff0000)
+    basic.pause(500)
+    basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00)
+    basic.pause(500)
+})
